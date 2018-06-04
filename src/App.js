78764fd36e4e36832605a22c6a5d07c42f378565
vybhavP <podalavybhav@gmail.com>
2018-06-04 23:14:02 +0530

    Added login and signup screens

--- src/App.js
+++ src/App.js
@@ -1,16 +1,56 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css';
-import { Button, Header, Icon, Image, Menu } from 'semantic-ui-react';
-import Menubar from './components/menubar.js'
-import MyImage from './images/home.jpg';
+// import { Button, Header, Icon, Image, Menu } from 'semantic-ui-react';
+// import Menubar from './components/menubar.js';
+// import ContainerExampleFluid from './components/container';
+// import DividerExampleHorizontal from './components/divider';
+// import MyImage from './images/home.jpg';
+
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import Home from './components/home';
+import About from './components/about';
+import Topics from './components/topics';
+import LoginOrSignup from './components/loginorsignup'
+import UserLogin from './components/user_login';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Menubar />
-        <Image src={MyImage} fluid />
+        {/* <Menubar /> */}
+        {/* <Image src={MyImage} fluid /> */}
+        <Router>
+    <div>
+      <ul className="Home_menu">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/topics">Topics</Link>
+        </li>
+      </ul>
+
+      <hr />
+
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={LoginOrSignup} />
+      <Route path="/about" component={About} />
+      <Route path="/topics" component={Topics} />
+      <Route path="/user_login" component={UserLogin} />
+
+    </div>
+  </Router>
+        {/* <ContainerExampleFluid /> */}
+        {/* <Button primary as={NavLink} to="/login">Login</Button> */}
+        {/* <DividerExampleHorizontal /> */}
       </div>
     );
   }
---
+++ src/components/loginorsignup.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Segment, Button, Divider } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+
+const LoginOrSignup = () => (
+    <Segment padded>
+        <Link to="/user_login">
+        <Button primary fluid>Login</Button>            
+        </Link>
+        <Divider horizontal>Or</Divider>
+        <Link to="/signup">
+        <Button secondary fluid>Sign Up Now</Button>
+        </Link>
+    </Segment>
+)
+
+export default LoginOrSignup
